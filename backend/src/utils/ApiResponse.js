fa3276b3d0d56f052fbb6d4fd0ea9af918284089
varun jain <varunjain.jain.j@gmail.com>
2026-06-11 02:53:00 +0530

    feat: utils module & error Middleware

---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+  constructor(statusCode, data, massage = "success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.massage = massage;
+    this.success = statusCode < 400;
+  }
+};
+
+module.exports = ApiResponse;
