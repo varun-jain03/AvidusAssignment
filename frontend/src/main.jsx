19155cf9e658e83553cee4d8c9e75ddb5049c9f0
varun jain <varunjain.jain.j@gmail.com>
2026-06-12 02:00:21 +0530

    feat: implement authentication ui with redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
 // Dependencies
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
 // File Imports
-// import "./index.css";
+import "./index.css";
 import App from "./App.jsx";
-// import { store } from "./app/store.js";
+import { store } from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </StrictMode>,
 );
---
+++ frontend/src/redux/auth/authThunk.js
@@ -0,0 +1,55 @@
+// Dependencies
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// File Imports
+import { BASE_URL } from "../../api/api.js";
+
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (userData, thunkAPI) => {
+    try {
+      const response = await fetch(
+        `${BASE_URL}/auth/register`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(userData)
+        }
+      );
+      const data = await response.json();
+      
+      if (!response.ok) {
+        throw new Error(data.message);
+      };
+      return data;
+    } 
+    catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+)
+
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (userData, thunkAPI) => {
+    try {
+      const response = await fetch(
+        `${BASE_URL}/auth/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(userData)
+        }
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+)
